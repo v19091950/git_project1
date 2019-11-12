c23b7c4df521eaa87b33fa554ac9ddadaa5e1f18
v19091950 <46817724+v19091950@users.noreply.github.com>
2019-11-12 16:40:47 +0300

    Update program.py
    commit frim github

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('UFO came and added this line')
+    print('Edit in GitHub')
 
 
 if __name__ == '__main__':
